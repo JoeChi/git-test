1e35f9ff19ea360470f0c23ae54942fdfc955ba4
chow <chow@gmail.com>
2022-06-24 18:07:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author chow_joe
+ * @create 2022/6/24 16:50
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
